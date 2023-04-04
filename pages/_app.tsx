59de647525df9fd9a8229f3dcbb243cc4c07f8dc
HanSeongLee <jkhanseong@gmail.com>
2023-04-04 19:38:00 +0900

    ADD | Font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  weight: ['400', '700', '800'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <>
+        <style jsx global>{`
+          html {
+            font-family: ${poppins.style.fontFamily};
+          }
+        `}</style>
+        <Component {...pageProps} />
+      </>
+  );
 }
 
 export default MyApp
