1df9c94018959f03054bf1117b35b65825e6d704
HanSeongLee <jkhanseong@gmail.com>
2023-04-06 18:10:00 +0900

    ADD | Error messages

---
+++ types/message.ts
@@ -0,0 +1,8 @@
+export enum ERROR_MESSAGES {
+    FIELD_REQUIRED='This field is required',
+    NOT_VALID_DAY='Must be a valid day',
+    NOT_VALID_MONTH='Must be a valid month',
+    NOT_VALID_YEAR='Must be a valid year',
+    NOT_FUTURE_YEAR='Must be in the past',
+    NOT_VALID_DATE='Must be a valid date',
+};
