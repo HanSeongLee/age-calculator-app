769070e4de5a8e1ec52ae90031478a64cca04524
HanSeongLee <jkhanseong@gmail.com>
2023-04-06 18:10:54 +0900

    ADD | Rules and error message to Input component

--- components/commons/Input/index.tsx
+++ components/commons/Input/index.tsx
@@ -1,24 +1,43 @@
 import React, { InputHTMLAttributes } from 'react';
 import styles from './style.module.scss';
 import { Control, useController } from 'react-hook-form';
+import { RegisterOptions } from 'react-hook-form/dist/types/validator';
+import cn from 'classnames';
 
 interface IProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'name'> {
     name: string;
     control: Control<any>;
+    rules?: Omit<RegisterOptions, 'valueAsNumber' | 'valueAsDate' | 'setValueAs' | 'disabled'>;
 }
 
-const Input: React.FC<IProps> = ({ control, name, className, ...props }) => {
-    const { field } = useController({
+const Input: React.FC<IProps> = ({
+                                     control, name, rules, className,
+                                     id, ...props
+                                 }) => {
+    const { field, fieldState: { error } } = useController({
         name,
+        rules,
         control,
     });
     return (
-        <input className={styles.input}
-               name={name}
-               value={field.value}
-               onChange={field.onChange}
-               {...props}
-        />
+        <div className={cn(styles.inputWrapper, {
+            [styles.error]: error,
+        }, className)}
+        >
+            <label htmlFor={id}>
+                {name}
+            </label>
+            <input className={styles.input}
+                   id={id}
+                   name={name}
+                   value={field.value}
+                   onChange={field.onChange}
+                   {...props}
+            />
+            <div className={styles.errorMessage}>
+                {error?.message}
+            </div>
+        </div>
     );
 };
 
