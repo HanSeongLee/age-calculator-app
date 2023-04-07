3423e9b66390109a042e7282b3c5a66297085470
HanSeongLee <jkhanseong@gmail.com>
2023-04-07 17:25:19 +0900

    ADD | Main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,23 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+import styles from './style.module.scss';
+import AgeCalculatorContainer from 'containers/AgeCalculatorContainer';
+import Container from 'components/commons/Container';
 
 const Home: NextPage = () => {
-  return (
-      <>
-        <main>
-        </main>
-      </>
-  );
-}
+    return (
+        <>
+            <header>
+                <h1 className={styles.hidden}>
+                    {process.env.NEXT_PUBLIC_TITLE}
+                </h1>
+            </header>
+            <main className={styles.main}>
+                <Container>
+                    <AgeCalculatorContainer />
+                </Container>
+            </main>
+        </>
+    );
+};
 
 export default Home
